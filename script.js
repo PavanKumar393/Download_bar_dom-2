495a95a4abe710fb690c50d30263fd1daee4421e
pavan_v <veligarampavan3@gmail.com>
2025-11-23 12:56:19 +0530

    download-bar-dom

---
+++ script.js
@@ -0,0 +1,37 @@
+let btn = document.querySelector("button");
+
+let inner = document.querySelector("#inner");
+
+let h1 = document.querySelector("h1");
+
+let success = document.querySelector("#success");
+
+let i = 0;
+inner.style.width = 0 + "%";
+
+btn.addEventListener("click", () => {
+  let time = setInterval(() => {
+    // console.log(i++);
+    i++;
+    console.log(i);
+    btn.style.pointerEvents = "none";
+    h1.innerHTML = i + "%";
+
+    if (i === 100) {
+      btn.innerText = "Downloded";
+      btn.style.opacity = 0.6;
+      btn.style.backgroundColor = "green";
+      btn.style.pointerEvents = "none";
+      success.style.display = "block";
+    }
+
+    inner.style.width = i + "%";
+  }, 100);
+
+  if ((inner.style.width = 100 + "%")) {
+    setTimeout(() => {
+      clearInterval(time);
+    }, 10000);
+    i = 0;
+  }
+});
